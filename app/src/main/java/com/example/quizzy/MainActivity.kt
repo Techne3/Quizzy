6bfddd664768d7a2d5ea6d663d11997e6314edac
Timothy Sharkey <Techne3@gmail.com>
2022-01-23 14:25:11 -0500

    Layout for questions fixed

--- app/src/main/java/com/example/quizzy/MainActivity.kt
+++ app/src/main/java/com/example/quizzy/MainActivity.kt
@@ -3,4 +3,17 @@ package com.example.quizzy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        if(savedInstanceState == null){
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container_view, QuestionsFragment() )
+                .commit()
+        }
+    }
+}
--- app/src/main/java/com/example/quizzy/QuestionsFragment.kt
+++ app/src/main/java/com/example/quizzy/QuestionsFragment.kt
@@ -1,6 +1,43 @@
 package com.example.quizzy
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.quizzy.databinding.QustionLayoutBinding
 
 class QuestionsFragment :Fragment() {
+
+
+    private var _binding: QustionLayoutBinding? = null
+    private val binding: QustionLayoutBinding get() = _binding!!
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = QustionLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+
+
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
 }
